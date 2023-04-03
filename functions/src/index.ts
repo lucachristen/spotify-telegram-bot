e8f9db851f147113c44ba23af7a11e2e9c652aba
Luca Christen <lucachristen94@gmail.com>
2023-04-03 20:59:24 +0200

    feat: Support new Spotify share link format

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,10 +3,10 @@ import axios from "axios";
 
 const {client_id, client_secret, refresh_token, playlist} =
   functions.config().spotify;
-// const playlistUrl = `https://open.spotify.com/playlist/${playlist}`;
-const trackRegex =
-    /https:\/\/open\.spotify\.com\/track\/([a-zA-Z0-9]{2,})(\S*)/g;
-
+const oldFormatRegex =
+  /https:\/\/open\.spotify\.com\/track\/([a-zA-Z0-9]{2,})(\S*)/g;
+const newFormatRegex =
+  /https:\/\/spotify\.link\/([a-zA-Z0-9]{2,})(\S*)/g;
 
 const handleUpdate = async (body: any) => {
   if (body?.message?.text) {
@@ -15,54 +15,94 @@ const handleUpdate = async (body: any) => {
 };
 
 const handleTrackMessage = async (message: string) => {
-  const trackUris = [...message.matchAll(trackRegex)]
-      .map((match) => `spotify:track:${match[1]}`);
+  const trackUris = await getTrackUris(message);
+
+  console.log({trackUris});
 
   if (trackUris.length > 0) {
-    try {
-      // Always get the fresh access token for each request (ugly af but who cares)
-      const tokenResponse = await axios.post("https://accounts.spotify.com/api/token",
-          {
-            "grant_type": "refresh_token",
-            "refresh_token": refresh_token,
-          },
-          {
-            headers: {
-              "Content-Type": "application/x-www-form-urlencoded",
-              "Authorization": `Basic ${Buffer.from(client_id+":"+client_secret).toString("base64")}`,
-            },
-          }
-      );
-      await axios.delete(
-          `https://api.spotify.com/v1/playlists/${playlist}/tracks`,
-          {
-            data: {
-              tracks: trackUris.map((uri) => {
-                return {uri};
-              }),
-            },
-            headers: {
-              "Content-Type": "application/json",
-              "Authorization": `Bearer ${tokenResponse.data.access_token}`,
-            },
-          });
-      await axios.post(
-          `https://api.spotify.com/v1/playlists/${playlist}/tracks`,
-          {
-            uris: trackUris,
-            position: 0,
-          },
-          {
-            headers: {
-              "Accept": "application/json",
-              "Content-Type": "application/json",
-              "Authorization": `Bearer ${tokenResponse.data.access_token}`,
-            },
-          });
-    } catch (err) {
-      console.error(err);
-    }
+    // Always get the fresh access token for each request (ugly af but who cares)
+    const accessToken = await getAccessToken();
+    await deleteIfAlreadyExists(trackUris, accessToken);
+    await addToPlaylist(trackUris, accessToken);
   }
+};
+
+const getTrackUris = async (message: string): Promise<string[]> => {
+  const oldFormatUris = [...message.matchAll(oldFormatRegex)].map(
+      (match) => `spotify:track:${match[1]}`
+  );
+
+  const trackUriPromises = [...message.matchAll(newFormatRegex)].map(
+      async (match) => {
+        const url = `https://spotify.link/${match[1]}`;
+        const response = await axios.get(url);
+        const uriMatches = [...(response.data?.matchAll(oldFormatRegex) ?? [])];
+        return uriMatches?.[0]?.[1] ?
+            `spotify:track:${uriMatches[0][1]}` :
+            undefined;
+      }
+  );
+
+  const newFormatUris = (await Promise.all(trackUriPromises))
+      .filter(Boolean)
+      .map((value) => value as string);
+
+  // Filter out duplicate values
+  return [...oldFormatUris, ...newFormatUris].filter(
+      (value, index, array) => array.indexOf(value) === index
+  );
+};
+
+const getAccessToken = async (): Promise<string> => {
+  const response = await axios.post(
+      "https://accounts.spotify.com/api/token",
+      {
+        grant_type: "refresh_token",
+        refresh_token: refresh_token,
+      },
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          "Authorization": `Basic ${Buffer.from(
+              client_id + ":" + client_secret
+          ).toString("base64")}`,
+        },
+      }
+  );
+  return response.data.access_token;
+};
+
+const deleteIfAlreadyExists = async (trackUris: string[], accessToken: string): Promise<void> => {
+  return await axios.delete(
+      `https://api.spotify.com/v1/playlists/${playlist}/tracks`,
+      {
+        data: {
+          tracks: trackUris.map((uri) => {
+            return {uri};
+          }),
+        },
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${accessToken}`,
+        },
+      }
+  );
+};
+
+const addToPlaylist = async (trackUris: string[], accessToken: string): Promise<void> => {
+  await axios.post(
+      `https://api.spotify.com/v1/playlists/${playlist}/tracks`,
+      {
+        uris: trackUris,
+        position: 0,
+      },
+      {
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${accessToken}`,
+        },
+      });
 };
 
 exports.bot = functions
